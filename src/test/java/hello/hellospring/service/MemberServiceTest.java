a814fd62b15bf56a61ef371f559f761bec15df25
dongwook <ehddnr1022@gmail.com>
2020-11-22 22:04:24 +0900

    SpringConfig를 통한 bean설정

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -28,24 +28,28 @@ public class MemberServiceTest {
 
     @Test
     public void 회원가입() throws Exception{
+        //given 뭔가 주어졌을 때
         Member member = new Member();
         member.setName("동우기");
 
+        //when 무엇을 검증하는가
         Long saveId = memberService.join(member);
 
+        //then 결과가 이렇게 나와야됨
         Member findMember = memberRepository.findById(saveId).get();
-
         assertEquals(member.getName(), findMember.getName());
 
     }
 
     @Test
     public void 중복회원예외() throws Exception{
+        //given
         Member member1 = new Member();
         member1.setName("김동욱");
         Member member2 = new Member();
         member2.setName("김동욱");
 
+        //when
         memberService.join(member1);
         //예외가 발생해야됨
         IllegalStateException e = assertThrows(IllegalStateException.class,()->memberService.join(member2));
