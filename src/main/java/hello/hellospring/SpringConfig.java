93bba4c568ef617a1b6ab87ce291641363a27d7c
dongwook <ehddnr1022@gmail.com>
2020-12-05 12:15:47 +0900

    스프링 통합 테스트

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,16 +1,30 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
 
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    //메모리 레파지토리에서 jdbc레파지토리로 쉽게 변경!!(다형성)
     @Bean
-    public MemoryMemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository(){
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
     @Bean
